1ad9b519a3068319657e807f4b24d1c0fe6191f5
Andron <andrssvin@gmail.com>
2024-09-16 01:24:33 +0300

    adding the 2nd lab

--- 3semJava/Labs/Lab2/StringFormatting.java
+++ 3semJava/Labs/Lab2/StringFormatting.java
@@ -1,18 +1,41 @@
 import java.util.StringTokenizer;
+import java.io.IOException;
+import java.lang.StringBuilder;
+
+class MyException extends Exception {
+    public static Exception WrongNumberOfArgs() {
+        return new Exception("wrong number of arguments");
+    }
+}
 
 public class StringFormatting {
-    public static void main(String[] args) {
-        StringTokenizer st = new StringTokenizer("this  66.85 is 998 a test");
-        while (st.hasMoreTokens()) {
-            String token = st.nextToken();
-            boolean flag = false;
-            try {
-                double d = Double.parseDouble(token);
-                if (token.indexOf('.') != -1) {
-                    flag = true;
-                }
-            } catch (NumberFormatException exc) {}
-            System.out.println(token + " - " + flag);
+    public static void main(String[] args) throws IOException {
+        try {
+            if (args.length == 0) {
+                throw MyException.WrongNumberOfArgs();
+            }
+            StringBuilder st = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                st.append(args[i]);
+                st.append(" ");
+            }
+            StringTokenizer sttok = new StringTokenizer(st.toString());
+            st.delete(0, st.length());
+            while (sttok.hasMoreTokens()) {
+                StringBuilder token = new StringBuilder(sttok.nextToken());
+                try {
+                    Double.parseDouble(token.toString());
+                    int i = token.indexOf(".");
+                    if (i != -1 && i + 2 < token.length()) {
+                        token = new StringBuilder(token.substring(0, i + 3));
+                    }
+                } catch (NumberFormatException e) {}
+                st.append(token.toString() + " ");
+            }
+            System.out.println(st.toString());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
         }
     }
 }
