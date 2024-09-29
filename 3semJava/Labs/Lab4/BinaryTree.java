a3814016b35d8ac1dbc5b55c2ad254aca103d40d
Andron <andrssvin@gmail.com>
2024-09-29 23:41:10 +0300

    adding picture with Person implementation

--- 3semJava/Labs/Lab4/BinaryTree.java
+++ 3semJava/Labs/Lab4/BinaryTree.java
@@ -1,3 +1,27 @@
+class Person implements Comparable<Person> {
+    String surname;
+    String name;
+
+    Person(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        int comp1 = this.surname.compareTo(p.surname);
+        if (comp1 != 0) {
+            return comp1;
+        }
+        return this.name.compareTo(p.name);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + surname + " " + name + ")";
+    }
+}
+
 public class BinaryTree<T extends Comparable<T>> {
     class Node {
         T value;
@@ -104,6 +128,7 @@ public class BinaryTree<T extends Comparable<T>> {
 
     void printVertLR() {
         printVertLR(root);
+        System.out.println();
     }
 
     void printVertLR(Node current) {
@@ -111,12 +136,13 @@ public class BinaryTree<T extends Comparable<T>> {
             return;
         }
         printVertLR(current.left);
-        System.out.println(current.value);
+        System.out.print(current.value.toString() + " ");
         printVertLR(current.right);
     }
 
     void printLRVert() {
         printLRVert(root);
+        System.out.println();
     }
 
     void printLRVert(Node current) {
@@ -125,11 +151,12 @@ public class BinaryTree<T extends Comparable<T>> {
         }
         printLRVert(current.left);
         printLRVert(current.right);
-        System.out.println(current.value);
+        System.out.print(current.value.toString() + " ");
     }
 
     void printLVertR() {
         printLVertR(root);
+        System.out.println();
     }
 
     void printLVertR(Node current) {
@@ -137,12 +164,11 @@ public class BinaryTree<T extends Comparable<T>> {
             return;
         }
         printLVertR(current.left);
-        System.out.println(current.value);
+        System.out.print(current.value.toString() + " ");
         printLVertR(current.right);
     }
 
     public static void main(String[] args) {
-        System.out.println("This is your tree : ");
         BinaryTree<Integer> tree = new BinaryTree<Integer>(5);
         tree.add(2);
         tree.add(8);
@@ -150,10 +176,31 @@ public class BinaryTree<T extends Comparable<T>> {
         tree.add(1);
         tree.add(6);
         tree.add(9);
+        System.out.println("This is your tree, path - vertex, left, right : ");
         tree.printVertLR();
+        System.out.println("Path - left, vertex, right : ");
+        tree.printLVertR();
+        System.out.println("Path - left, right, vertex : ");
+        tree.printLRVert();
         tree.add(-1);
+        tree.add(0);
+        System.out.println("Tree after adding -1, 0 : ");
         tree.printVertLR();
         tree.remove(5);
+        System.out.println("Removed 5, the root : ");
         tree.printLVertR();
+
+        BinaryTree<Person> pTree = new BinaryTree<Person>(new Person("Einstein", "Albert"));
+        pTree.add(new Person("Newton", "Isaak"));
+        pTree.add(new Person("Trump", "Judd"));
+        pTree.add(new Person("Armstrong", "Neil"));
+        System.out.println("Tree of people : ");
+        pTree.printVertLR();
+        if (pTree.find(new Person("Armstrong", "Louis"))) {
+            System.out.println("Armstrong Louis is here");
+        } else {
+            System.out.println("Armstrong Louis is not here");
+        }
+        System.out.println("Armstrong Neil is " + (pTree.find(new Person("Armstrong", "Neil")) ? "" : "not ") + "here");
     }
 }
